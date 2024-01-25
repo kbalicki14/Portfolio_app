a65bc8745e695b2c872160d34d0cfff18e873086
Chris <kbalicki2014@gmail.com>
2024-01-25 01:10:50 +0100

    deploy koyeb

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "core/static/",
 ]
 STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
