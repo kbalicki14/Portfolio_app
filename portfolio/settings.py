47fb49a0a74f6625331e88fc04c7790eb3326e2b
Chris <kbalicki2014@gmail.com>
2024-01-25 15:51:27 +0100

    https enable

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+SECURE_SSL_REDIRECT = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
