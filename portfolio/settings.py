61dc5e17b88b4776a12d15f1c0261e95fc43f1b4
Chris <kbalicki2014@gmail.com>
2024-01-25 16:03:07 +0100

    debug

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-SECURE_SSL_REDIRECT = True
+DEBUG = True
+# SECURE_SSL_REDIRECT = False
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
