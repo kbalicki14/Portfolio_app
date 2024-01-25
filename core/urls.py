6b0ff898616f846cbfa753ac6749171860e1a29b
Chris <kbalicki2014@gmail.com>
2024-01-25 20:50:49 +0100

    media

--- core/urls.py
+++ core/urls.py
@@ -54,6 +54,8 @@ urlpatterns = [
 
     # <int:advertiseModel_pk>
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
